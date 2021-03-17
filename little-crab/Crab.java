ac1ccd28d51987f7f00ef622724e584923e56407
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-03-17 13:51:15 +0100

    Add files via upload

---
+++ little-crab/Crab.java
@@ -0,0 +1,14 @@
+import greenfoot.*;
+
+/**
+ * Diese Klasse definiert eine Krabbe. Krabben leben am Strand.
+ */
+public class Crab extends Actor
+{
+    public void act()
+    {
+        // Füge hier deinen Code ein.
+    }
+}
+
+
